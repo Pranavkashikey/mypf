9937cf5aa1ceff5f0ac0f966ed0618dc8c346a95
Pranavkashikey <141570116+Pranavkashikey@users.noreply.github.com>
2026-01-17 17:02:27 +0530

    Update ResumePage.js

--- src/pages/resume_page/ResumePage.js
+++ src/pages/resume_page/ResumePage.js
@@ -2,7 +2,7 @@ import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 import Particle from "../../Particle";
-import pdf from "../../assets/myresume.pdf";
+// import pdf from "../../assets/myresume.pdf";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import Zoom from "react-reveal/Zoom";
